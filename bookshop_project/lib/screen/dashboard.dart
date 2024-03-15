d645d23bead0c5dad0c523f3a9c9a64b52acc195
Archer <ogunkapc@gmail.com>
2024-03-15 13:05:16 +0100

    overflow fixes

--- bookshop_project/lib/screen/dashboard.dart
+++ bookshop_project/lib/screen/dashboard.dart
@@ -17,7 +17,6 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-
   @override
   void initState() {
     searchController.addListener(() {
@@ -34,172 +33,188 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
-
-    return Consumer<IndexProvider>(builder: (context, provider, child){
-      final items = _searchText.isEmpty ? provider.items : provider.items.isEmpty
-          ? [] : provider.items.where((e) => e.title.toLowerCase().contains(_searchText.toLowerCase())).toList();
+    return Consumer<IndexProvider>(builder: (context, provider, child) {
+      final items = _searchText.isEmpty
+          ? provider.items
+          : provider.items.isEmpty
+              ? []
+              : provider.items
+                  .where((e) =>
+                      e.title.toLowerCase().contains(_searchText.toLowerCase()))
+                  .toList();
       return Scaffold(
         backgroundColor: Theme.of(context).colorScheme.surface,
         body: Padding(
           padding: const EdgeInsets.all(25),
           child: SingleChildScrollView(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                              const Text(
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const Text(
                               "Hi, ",
                               style: TextStyle(
-                              fontSize:15,
-                              color:Colors.black,
-                              fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                                  Text(
-                                    username,
-                                    style: const TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15,
-                                    ),
-                                  ),
-                              const SizedBox(width: 10,),
-                              Image.asset(
+                            Text(
+                              username,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Image.asset(
                               'assets/images/emoji_teeth.png',
                               height: 25,
                               width: 25,
                             ),
-                                ],
-                              ),
-                            Text(
-                              "Welcome to OneStop",
-                              style: TextStyle(
-                              fontSize:15,
-                              color:GlobalColors.linear,
-                              ),
-                            )
-                            ],
+                          ],
+                        ),
+                        Text(
+                          "Welcome to OneStop",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: GlobalColors.linear,
                           ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                          // go to cart
-                          const Cart();
-                          },
-                          child: Container(
-                          height: 28,
-                          width: 28,
-                          decoration: BoxDecoration(
-                          shape:BoxShape.circle,
-                          color: GlobalColors.smallTextColorGrey,
-                          ),
-                            child: Image.asset(
-                              'assets/images/cart_icon.png',
-                              // height: 10,
-                              // width: 10,
-                            ),
-                          ),
-                        ),
-                    ],
-                  ),
-                  const SizedBox(height: 15),
-
-                  //SEARCH BAR
-                  Container(
-                    height: 36,
-                    width: 257,
-                    padding: const EdgeInsets.only(top: 0, left: 16, bottom: 0, right: 0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(45),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 1,
-                      ),
-                    ),
-                    child: TextFormField(
-                      controller: searchController,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Search',
-                        hintStyle: const TextStyle(
-                          height: 1,
-                          fontSize: 14,
-                        ),
-                        suffixIcon: InkWell(
-                          onTap: () {
-                            ////search for course
-                          },
-                          child: const Icon(CupertinoIcons.search)
-                        ),
-                      ),
+                        )
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 20,),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
+                  InkWell(
+                    onTap: () {
+                      // go to cart
+                      const Cart();
+                    },
                     child: Container(
-                      // empty
-                    )
-                  ),
-
-                        const SizedBox(height: 20),
-
-                        if(items.isEmpty)
-                          const SearchNotFound()
-                        else
-                        //GRID OF ITEMS
-                        // ListView.separated(
-                        //     shrinkWrap: true,
-                        //       physics: const NeverScrollableScrollPhysics(),
-                        //       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
-                        //       itemBuilder: (context, index){
-                        //         final item = items[index];
-                        //         return ItemDesign(item: item, callback: (item){
-                        //           //Navigate to your next screen using the course object
-                        //           if(mounted) {
-                        //             Navigator.push(context, MaterialPageRoute(builder: (context) => const ItemDesc()));
-                        //           }
-                        //         });
-                        //       },
-                        //       separatorBuilder: (context, index){
-                        //         return const SizedBox(height: 14);
-                        //       },
-                        //       itemCount: items.length
-                        //   ),
-
-                  GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    //padding: const EdgeInsets.only(bottom: 100),
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 115, crossAxisSpacing: 10),
-// crossAxisSpacing -Gives space between the two columns
-// due to somepadding whom i don'tknow where is, there is already space betwwen them
-                    itemBuilder: (context, index){
-                                final item = items[index];
-                                return ItemDesign(item: item, callback: (item){
-                                  //Navigate to your next screen using the course object
-                                  if(mounted) {
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => const ItemDesc()));
-                                  }
-                                });
-                              },
-                                itemCount: items.length,
+                      height: 28,
+                      width: 28,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: GlobalColors.smallTextColorGrey,
+                      ),
+                      child: Image.asset(
+                        'assets/images/cart_icon.png',
+                        // height: 10,
+                        // width: 10,
+                      ),
                     ),
-                ]
-            ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+
+              //SEARCH BAR
+              Container(
+                height: 36,
+                width: 257,
+                padding: const EdgeInsets.only(
+                    top: 0, left: 16, bottom: 0, right: 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(45),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                ),
+                child: TextFormField(
+                  controller: searchController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search',
+                    hintStyle: const TextStyle(
+                      height: 1,
+                      fontSize: 14,
+                    ),
+                    suffixIcon: InkWell(
+                        onTap: () {
+                          ////search for course
+                        },
+                        child: const Icon(CupertinoIcons.search)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                      // empty
+                      )),
+
+              const SizedBox(height: 20),
+
+              if (items.isEmpty)
+                const SearchNotFound()
+              else
+                //GRID OF ITEMS
+                // ListView.separated(
+                //     shrinkWrap: true,
+                //       physics: const NeverScrollableScrollPhysics(),
+                //       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                //       itemBuilder: (context, index){
+                //         final item = items[index];
+                //         return ItemDesign(item: item, callback: (item){
+                //           //Navigate to your next screen using the course object
+                //           if(mounted) {
+                //             Navigator.push(context, MaterialPageRoute(builder: (context) => const ItemDesc()));
+                //           }
+                //         });
+                //       },
+                //       separatorBuilder: (context, index){
+                //         return const SizedBox(height: 14);
+                //       },
+                //       itemCount: items.length
+                //   ),
+
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  // padding: const EdgeInsets.only(bottom: 100),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 115,
+                    crossAxisSpacing: 30,
+                  ),
+                  // crossAxisSpacing -Gives space between the two columns
+                  // due to somepadding whom i don'tknow where is, there is already space betwwen them
+                  itemBuilder: (context, index) {
+                    final item = items[index];
+                    return ItemDesign(
+                        item: item,
+                        callback: (item) {
+                          //Navigate to your next screen using the course object
+                          if (mounted) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const ItemDesc(),
+                              ),
+                            );
+                          }
+                        });
+                  },
+                  itemCount: items.length,
+                ),
+            ]),
           ),
         ),
       );
     });
   }
-
 }
--- bookshop_project/lib/utils/store_items/item_design_global.dart
+++ bookshop_project/lib/utils/store_items/item_design_global.dart
@@ -12,63 +12,78 @@ import 'package:flutter/material.dart';
 typedef MyCallback = Function(Item item);
 
 class ItemDesign extends StatelessWidget {
-  const ItemDesign({Key? key, required this.item, required this.callback}) : super(key: key);
+  const ItemDesign({
+    Key? key,
+    required this.item,
+    required this.callback,
+  }) : super(key: key);
   final Item item;
   final MyCallback callback;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
+      onTap: () {
         callback(item);
       },
       child: Container(
-       // width: 160,
-       // height: 400,
+        height: 200,
+        width: 150,
         decoration: BoxDecoration(
-color: Colors.lightBlueAccent,
-        border: Border.all(
-        color: Colors.black,width:2,
-        style:BorderStyle.solid,
-        )
+          border: Border.all(
+              // color: Colors.black,
+              // width: 1,
+              ),
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             //IMAGE
-            Container(
-              //height: 203,
-              width: 160,
-              decoration: BoxDecoration(
-                borderRadius:  BorderRadius.circular(17),
-                border: Border.all(
-        color: Colors.black,width:2,
-        style:BorderStyle.solid,
-        )
-              ),
-              child: Image.asset(
-                        item.image,
-                        fit: BoxFit.contain,
-                      ),
-            ),
-            const SizedBox(height: 13,),
-            //TITLE
-                  Text(
-                    item.title,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 11,
-                      fontWeight: FontWeight.bold,
-                    ),
+            Flexible(
+              child: Container(
+                width: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(17),
+                  border: Border.all(
+                    color: Colors.red,
+                    width: 2,
+                    style: BorderStyle.solid,
                   ),
-                  const SizedBox(height: 14),
-                  //PRICE
-                  Text(
-                          '\u20a6${item.price.toStringAsFixed(2)}',
-                          style: const TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(17),
+                  child: Image.asset(
+                    item.image,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            //TITLE
+            Text(
+              item.title,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 11,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            //PRICE
+            Text(
+              '\u20a6${item.price.toStringAsFixed(2)}',
+              style: const TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
           ],
         ),
       ),
